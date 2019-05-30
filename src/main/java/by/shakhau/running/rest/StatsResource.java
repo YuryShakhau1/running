6003bbdc9374c6460f4c418276fdc70d45fc3965
Yury Shakhau <yury_shakhau@mail.ru>
2019-05-30 13:33:53 +0300

    Authorization token is added to swagger ui

--- src/main/java/by/shakhau/running/rest/StatsResource.java
+++ src/main/java/by/shakhau/running/rest/StatsResource.java
@@ -3,6 +3,8 @@ package by.shakhau.running.rest;
 import by.shakhau.running.service.RunningStatsService;
 import by.shakhau.running.service.dto.AverageStats;
 import by.shakhau.running.service.dto.Stats;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
@@ -17,32 +19,50 @@ public class StatsResource {
     private RunningStatsService runningStatsService;
 
     @GetMapping(value = "/all", produces = MediaType.APPLICATION_JSON_VALUE)
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "Authorization", value = "Authorization token",
+                    required = true, dataType = "string", paramType = "header") })
     public List<Stats> findAll() {
         return runningStatsService.findAll();
     }
 
     @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "Authorization", value = "Authorization token",
+                    required = true, dataType = "string", paramType = "header") })
     public Stats findAll(@PathVariable Long id) {
         return runningStatsService.findById(id);
     }
 
     @GetMapping(value = "/average/user/{userId}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "Authorization", value = "Authorization token",
+                    required = true, dataType = "string", paramType = "header") })
     public List<AverageStats> average(@PathVariable Long userId) {
         return runningStatsService.averageStats(userId);
     }
 
     @PostMapping(value = "/add/user/{userId}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "Authorization", value = "Authorization token",
+                    required = true, dataType = "string", paramType = "header") })
     public Stats add(@PathVariable Long userId, @RequestBody Stats stats) {
         return runningStatsService.add(stats, userId);
     }
 
     @PutMapping(value = "/update",
             consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "Authorization", value = "Authorization token",
+                    required = true, dataType = "string", paramType = "header") })
     public Stats update(@RequestBody Stats stats) {
         return runningStatsService.update(stats);
     }
 
     @DeleteMapping(value = "/{id}")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "Authorization", value = "Authorization token",
+                    required = true, dataType = "string", paramType = "header") })
     public void delete(@PathVariable Long id) {
         runningStatsService.delete(id);
     }
--- src/main/java/by/shakhau/running/rest/UserResource.java
+++ src/main/java/by/shakhau/running/rest/UserResource.java
@@ -4,6 +4,8 @@ import by.shakhau.running.security.UserAuthenticationDto;
 import by.shakhau.running.service.UserService;
 import by.shakhau.running.service.dto.Stats;
 import by.shakhau.running.service.dto.User;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.Authentication;
@@ -19,6 +21,9 @@ public class UserResource {
     private UserService userService;
 
     @GetMapping(value = "/all", produces = MediaType.APPLICATION_JSON_VALUE)
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "Authorization", value = "Authorization token",
+                    required = true, dataType = "string", paramType = "header") })
     public List<User> findAll() {
         return userService.findAll();
     }
